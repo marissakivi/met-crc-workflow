9e89b6309b3736acfc81d9faff2ca6b00db285a3
Marissa Kivi <mkivi@crcfe01.crc.nd.edu>
2019-08-16 13:33:48 -0400

    new prism files for sites, unsure if we should be keeping all of these, but might as well for now

---
+++ jobs/step_7.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -M mkivi@nd.edu
+#$ -m abe
+#$ -pe smp 1
+#$ -q long
+#$ -N test
+
+module load R
+module load netcdf/4.7.0/intel/18.0
+module load gdal/3.0.0/gcc/4.8.5 
+module load geos
+
+R CMD BATCH ~/met-crc-workflow/scripts/STEP-7-prism.R  output_7.out
