ec30f436540bbe5c5835c378ad23d93da099f9d5
Marissa Kivi <mkivi@crcfe02.crc.nd.edu>
2019-08-23 14:21:51 -0400

    step two fixes

--- jobs/step_2.sh
+++ jobs/step_2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #$ -M mkivi@nd.edu
 #$ -m abe
-#$ -pe smp 1 
+#$ -pe smp 4 
 #$ -q long 
 #$ -N step2
 
 module load R
 module load netcdf/4.7.0/intel/18.0
 
-R CMD BATCH ~/met/scripts/STEP-2-debias.R  output_2.out
+R CMD BATCH ~/met-crc-workflow/scripts/STEP-2-debias.R  output_2.out
